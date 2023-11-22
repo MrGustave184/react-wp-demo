ff98f9429adab57f87b0499ac0de6bc8aa218e92
Rafael Ortiz <rafaelortiz@rainpos.com>
2023-11-22 10:39:56 -0400

    enqueue wp react

--- wp-content/themes/react-demo/functions.php
+++ wp-content/themes/react-demo/functions.php
@@ -1,3 +1,14 @@
 <?php
 
-// 
+add_action( 'wp_enqueue_scripts', 'wp_react_demo_scripts' );
+
+function wp_react_demo_scripts()
+{
+    wp_enqueue_script(
+        'my-theme-frontend',
+        get_stylesheet_directory_uri() . '/build/index.js',
+        ['wp-element'],
+        time(), // for caching, go null for prod
+        true
+    );
+}
