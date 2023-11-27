70bf3ef224394dbceecd1ac5986b0bf997268caa
Rafael Ortiz <rafaelortiz@rainpos.com>
2023-11-27 14:16:39 -0400

    wp modules and register student post type

--- wp-content/themes/react-demo/functions.php
+++ wp-content/themes/react-demo/functions.php
@@ -1,12 +1,29 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'wp_react_demo_scripts' );
+// Register JS scripts in the wordpress core
+add_action('wp_enqueue_scripts', 'wp_react_demo_scripts');
+
+// Register the student custom post type
+add_action('init', 'wporg_custom_post_type');
+
+function wporg_custom_post_type()
+{
+	register_post_type('student', [
+			'labels'      => array(
+				'name'          => __('Products', 'textdomain'),
+				'singular_name' => __('Product', 'textdomain'),
+			),
+				'public'      => true,
+				'has_archive' => true,
+		]
+	);
+}
 
 function wp_react_demo_scripts()
 {
     wp_enqueue_script(
         'react-demo-scripts',
-        get_stylesheet_directory_uri() . '/build/index.js',
+        get_theme_file_uri('/build/index.js'),
         ['wp-element'],
         time(), // for caching, go null for prod
         true
